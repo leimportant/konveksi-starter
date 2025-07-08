a41200b7a02a83ba91fb14f5561584db8050b65a
m_soleh <mohamad.soleh@nabatisnack.co.id>
2025-07-08 17:09:07 +0700

    fix send message

--- resources/js/pages/Order/OrderRequest.vue
+++ resources/js/pages/Order/OrderRequest.vue
@@ -95,6 +95,7 @@ const handleFilter = (e: Event) => {
     });
 
 const openMessageModal = (ids: string[], targetReceiverId: number) => {
+    console.log(targetReceiverId);
     receiverId.value = targetReceiverId;
     selectedOrders.value = ids;
     showMessageModal.value = true;
@@ -285,7 +286,7 @@ async function submitShipping() {
                                             class="absolute left-0 top-full z-50 mt-2 w-44 rounded-md bg-white dark:bg-gray-800 text-black dark:text-white shadow-lg ring-1 ring-black ring-opacity-5">
                                             <div class="py-1 text-left text-sm">
                                                 <Button variant="ghost" size="icon"
-                                                    @click="openMessageModal([order.id], order.customer_id)"
+                                                    @click="openMessageModal([order.id], order.customer?.id)"
                                                     class="w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
                                                     Kirim Pesan
                                                 </Button>
