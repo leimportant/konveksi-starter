2a55362fdd3ba3083547a06a984806fcd02d1d15
m_soleh <mohamad.soleh@nabatisnack.co.id>
2025-09-02 11:51:40 +0700

    redirect chatbot

---
+++ resources/js/pages/home/ChatbotPage2.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+const assistantUrl = import.meta.env.VITE_ASSISTANT_URL;
+</script>
+
+
+<template>
+   <Head title="Assistant Chat" />
+    <AppLayout>
+      <div class="w-full h-screen">
+        <iframe 
+           :src="assistantUrl || 'http://localhost:3000'"
+          class="w-full h-full border-0"
+        ></iframe>
+      </div>
+    </AppLayout>
+  
+</template>
